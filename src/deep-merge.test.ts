50e45eadb2c7f91bc851eaba5c09544f35d149d4
Andrii Kirmas <kirmas@phystech.edu>
2021-10-17 00:41:05 +0300

    Simplify tests

--- src/deep-merge.test.ts
+++ src/deep-merge.test.ts
@@ -1,157 +1,57 @@
 import { deepMerge } from "./deep-merge"
 
 describe(deepMerge.name, () => {
-  describe("simple", () => {
-    const source = {"a": "a"}
-    it("same", () => expect(deepMerge(source, source)).toBe(source))
-    it("no patch", () => expect(deepMerge(source, undefined)).toBe(source))
-  })
+  it("primitive", () => expect(deepMerge("a", "b")).toBe("b"))
 
-  it("scalar", () => expect(deepMerge("a", "b")).toBe("b"))
+  describe("singleton", () => {
+    const source = {"a": "a"}
+
+    it("same", () => expect(deepMerge(source, source)).toBe(source))
+    it("clone", () => expect(deepMerge(source, {...source})).toBe(source))
+    it("empty", () => expect(deepMerge(source, {})).toBe(source))
+    it("no patch", () => expect(deepMerge(source, undefined)).toBe(source))
+    it("patch", () => expect(deepMerge({} as typeof source, source)).toBe(source))
+  })
 
   describe("array", () => {
     const arr = ["a"]
     , p1 = ["b"]
-    , difLength = ["a", "b"]
+    , more = ["a", "b"]
 
-    it("same", () => expect(deepMerge(arr,
-      ["a"]
-    )).toBe(
-      arr
-    ))
-
-    it("overwrite", () => expect(deepMerge(arr,
-      p1
-    )).toBe(
-      p1
-    ))
-
-    it("overwrite with patch", () => expect(deepMerge(arr,
-      difLength
-    )).toBe(
-      difLength
-    ))
+    it("clone", () => expect(deepMerge(arr, ["a"])).toBe(arr))
+    it("overwrite", () => expect(deepMerge(arr, p1)).toBe(p1))
+    it("patch", () => expect(deepMerge(arr, more)).toBe(more))
   })
 
-
-  describe("flat assoc", () => {
-    const source: {
-      "a": number
-      "b"?: number
-      "c": number
-      "d"?: number
-      "e"?: number
-    } = {"a": 1, "b": 2, "c": 3}
-
-    it("subset", () => expect(deepMerge(source, {
-      "a": 1
-    })).toBe(
-      source
-    ))
-
-    it("delete", () => expect(deepMerge(source, {
-      "b": null
-    })).toStrictEqual({
-      "a": 1,
-      "c": 3
-    }))
-
-    it("merge", () => expect(deepMerge(source, {
-      "a": 1,
-      "c": 3
-    })).toStrictEqual({
-      "a": 1,
-      "b": 2,
-      "c": 3
-    }))
-
-    it("merge with undefined", () => expect(deepMerge(source, {
-      "a": 1,
-      "b": undefined,
-      "c": 3,
-      "d": null,
-      "e": 5
-    })).toStrictEqual({
-      "a": 1,
-      "b": 2,
-      "c": 3,
-      "e": 5
-    }))
-  })
-
-  describe("deep", () => {
-    const deep: {n: {n: {
-      a?: string
-      b?: string
-      c?: string
-    }}} = {
-      "n": {
-        "n": {
-          "a": "a",
-          "c": "c"
-        }
-      }
-    }
-    , overwrite: typeof deep = {
-      "n": {
-        "n": {
-          "a": "A",
-          "c": "c"
-        }
-      }
-    }  
-    , patch: Shredded<typeof deep> = {
-      "n": {
-        "n": {
-          "b": "b"
-        }
-      }
-    }
-    , sub: Shredded<typeof deep> = {
-      "n": {
-        "n": {
-          "a": "a"
-        }
-      }
+  it("assoc", () => {
+    const s = {
+      "same": "same",
+      "untouched": "untouched",
+      "undefined": "undefined",
+      "delete": "delete",
+      "modify": "1" as string|number,
+      "overwrite": {"0": 0}
+    } as const
+    , source = s as Partial<typeof s & {"add": "add"}>
+    , patch = {
+      "same": "same",
+      "undefined": undefined,
+      "delete": null,
+      "already deleted": null,
+      "modify": 1,
+      "overwrite": [0],
+      "add": "add",
+    } as const
+    , merged = {
+      "same": "same",
+      "untouched": "untouched",
+      "undefined": "undefined",
+      "modify": 1,
+      "overwrite": [0],
+      "add": "add"
     }
 
-    it("sub", () => expect(deepMerge(deep, sub)).toBe(deep))
-
-    it("overwrite", () => expect(deepMerge(deep, overwrite)).toBe(overwrite))
-
-    it("update", () => expect(deepMerge(deep, patch)).toStrictEqual({
-      "n": {
-        "n": {
-          "a": "a",
-          "b": "b",
-          "c": "c"
-        }
-      }
-    }))
-
-    describe("delete", () => {
-      const delete_patch: Shredded<typeof deep> = {
-        "n": {
-          "n": {
-            "a": null,
-            "c": null
-          }
-        }
-      }
-
-      it("option1", () => expect(deepMerge(deep, delete_patch)).toStrictEqual(
-        {"n": {"n": {}}}
-      ))
-      it("not nested empty", () => expect(deepMerge(deep, delete_patch)).not.toStrictEqual(
-        {"n": {}}
-      ))
-      it("not empty object", () => expect(deepMerge(deep, delete_patch)).not.toStrictEqual(
-        {}
-      ))
-      it("not undefined", () => expect(deepMerge(deep, delete_patch)).not.toBe(
-        undefined
-      ))
-    })
-
-  })
+    expect(deepMerge(source, patch)).toStrictEqual(merged)
+    expect(deepMerge({"deep": source}, {"deep": patch})).toStrictEqual({"deep": merged})
+  })  
 })
