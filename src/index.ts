081a6e44005b31bbd943d31141c656cb9ec1d5db
Andrii Kirmas <kirmas@phystech.edu>
2021-10-16 22:04:22 +0300

    Remove redundant stuff

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
 /* istanbul ignore file */
 export {default as deepMerge} from "./deep-merge"
-export {default as diffMap, DiffStatus} from "./diff-map"
-export {default as diffYamled} from "./diff-yamled"
