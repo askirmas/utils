d6685d4f419fcdcc64107ab2e0da0682e78ca501
Andrii Kirmas <kirmas@phystech.edu>
2022-04-23 15:20:56 +0300

    Add `project` function with bad notation

--- src/object.test.ts
+++ src/object.test.ts
@@ -1,26 +1,66 @@
-import { pick } from "./object";
+import {
+  pick,
+  project
+} from "./object";
 
 describe(pick.name, () => {
-  
-  describe("array", () => {
-    const source: Partial<Record<"a"|"b"|"c", number>> = {"a": 1, "b": 1}
+  const source: Partial<Record<"a"|"b"|"c", number>> = {"a": 1, "b": 1}
 
-    it("no target", () => expect(pick(
+  it("no target", () => {
+    const result = pick(
       source,
       ["a", "c"]
-    )).toStrictEqual({
+    )
+
+    expect(result).toStrictEqual({
       "a": 1,
       "c": undefined
-    }))
-    
-    it("with target", () => expect(pick(
-      source,
-      ["a", "c"],
-      {"a": 2, "b": 2, "c": 2} as typeof source
-    )).toStrictEqual({
-      "a": 1,
-      "b": 2,
-      "c": undefined
-    }))
+    })
+  })
+  
+  it("with target", () => expect(pick(
+    source,
+    ["a", "c"],
+    {"a": 2, "b": 2, "c": 2} as typeof source
+  )).toStrictEqual({
+    "a": 1,
+    "b": 2,
+    "c": undefined
+  }))
+})
+
+describe(project.name, () => {
+  it("demo", () => {
+    const obj = {
+      "keep": "Keep",
+      "remove": "Remove",
+      "rename": "Rename",
+      "omit": "Omit",
+      "replace": "Replace",
+      "copy": "Copy",
+    }
+    , result = project(obj, {
+      //@ts-expect-error
+      "keep": 1,
+      //@ts-expect-error
+      "remove": 0,
+      "renamed": "rename",
+      //@ts-expect-error
+      "replace": false,
+      "replaced": "replace",
+      "copy1": "copy",
+      //@ts-expect-error
+      "copy": "copy",
+      "copy2": "copy",
+    })
+
+    expect(obj).toBe(result)
+    expect(result).toStrictEqual({
+      "keep": "Keep",
+      "renamed": "Rename",
+      "replaced": "Replace",
+      "copy1": "Copy",
+      "copy2": "Copy",
+    })
   })
 })
--- src/object.ts
+++ src/object.ts
@@ -1,7 +1,13 @@
-import type { SubKeys } from "./ts-swiss.types"
+import type {
+  Dict,
+  SubKeys
+} from "./ts-swiss.types"
+
+const {isArray} = Array
 
 export {
-  pick
+  pick,
+  project
 }
 
 function pick<S, P extends Array<keyof S>>(
@@ -20,5 +26,63 @@ function pick(source: unknown, picking: unknown[], target = {}) {
     target[key] = source[key]
   }
   
+  return target
+}
+
+function project<S,
+  Pick extends {[s in keyof S]?: 1|true},
+  Omit extends {[s in keyof S]?: 0|false|null|undefined},
+  Rename extends {[k: string]: keyof S}
+  >(base: S, map: Pick & Omit) {
+  const target = base as unknown as {
+    [k in keyof Pick & keyof Rename]:
+      k extends Extract<keyof Pick, keyof S> ? S[k]
+      : k extends keyof Rename ? S[Rename[k]] 
+      : never
+  }
+  , toKeep = new Set<string>()
+  , renames: Dict<string | string[]> = {}
+  
+  for (const key in map) {
+    const proj = map[key as keyof typeof map]
+    
+    if (typeof proj === "string") {
+      const pre = renames[proj]
+      
+      if (pre === undefined)
+        renames[proj] = key
+      else if (isArray(pre))
+        pre.push(key)
+      else
+        renames[proj] = [pre, key]
+
+    } else if (proj)
+      toKeep.add(key)
+  }
+
+  for (const key in base) {
+    const rename = renames[key]
+
+    if (rename !== undefined) {
+      if (!isArray(rename))
+        //@ts-expect-error
+        target[rename] = base[key]
+
+      else {
+        const value = base[key]
+        , {length} = rename
+  
+        toKeep.has(key) || delete target[key]
+  
+        for (let i = 0; i < length; i++)
+          //@ts-expect-error
+          target[rename[i]] = value
+      }
+    }
+
+    if (!toKeep.has(key))
+      delete target[key]
+  }
+    
   return target
 }
